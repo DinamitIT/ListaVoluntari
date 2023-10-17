cbbd753f540e9d2776dd2e55ba090cabda76832a
xRankk <calin.silvian@gmail.com>
2023-10-17 23:34:19 +0300

    h1 si numele meu

--- client/src/App.js
+++ client/src/App.js
@@ -6,23 +6,23 @@ function App() {
       <div className="App-header">
         <div className="titlu">Voluntari Departament IT 2023-2024</div>
         <div className="scrollable-div">
-          <h4>Nume si prenume - Nume github</h4>
-          <h4>Theo Storm - $efu</h4>
-          <h4>Cristi Milmoiu - Technic bo$$</h4>
-          <h4>Boscanici Adrian - O secreta pe mata</h4>
-          <h4>Dulgheriu Bogdan Gabriel - dulgheriubogdan</h4>
-          <h4>test - test</h4>
-          <h4>test - test</h4>
-          <h4>test - test</h4>
-          <h4>test - test</h4>
-          <h4>test - test</h4>
-          <h4>test - test</h4>
-          <h4>test - test</h4>
-          <h4>test - test</h4>
-          <h4>test - test</h4>
-          <h4>test - test</h4>
-          <h4>test - test</h4>
-          <h4>test - test</h4>
+          <h1>Nume si prenume - Nume github</h1>
+          <h1>Theo Storm - $efu</h1>
+          <h1>Cristi Milmoiu - Technic bo$$</h1>
+          <h1>Boscanici Adrian - O secreta pe mata</h1>
+          <h1>Dulgheriu Bogdan Gabriel - dulgheriubogdan</h1>
+          <h1>Calin Silvian Petre - xRankk</h1>
+          <h1>test - test</h1>
+          <h1>test - test</h1>
+          <h1>test - test</h1>
+          <h1>test - test</h1>
+          <h1>test - test</h1>
+          <h1>test - test</h1>
+          <h1>test - test</h1>
+          <h1>test - test</h1>
+          <h1>test - test</h1>
+          <h1>test - test</h1>
+          <h1>test - test</h1>
         </div>
       </div>
     </div>
