d0736c8e745c0fcf032e5b300b43e59b86e49e49
dulgheriubogdan <dulgheriubogdan30@gmail.com>
2023-10-18 23:02:02 +0300

    Added href to github name and added tailwind

--- client/src/ListaVoluntari.js
+++ client/src/ListaVoluntari.js
@@ -3,10 +3,28 @@ import "./App.css";
 function ListaVoluntari() {
   return (
     <div className="scrollable-div">
-      <h1>Nume Prenume - Nume GitHub</h1>
-      <h1>Boscanici Adrian - O secreta pe mama ta</h1>
-      <h1>Dulgheriu Bogdan Gabriel - dulgheriubogdan</h1>
-      <h1>Calin Silvian Petre - xRankk</h1>
+      <h1 class=" cursor-s-resize">Nume Prenume - Nume GitHub</h1>
+      <h1>
+        Dulgheriu Bogdan Gabriel -
+        <a
+          href="https://github.com/dulgheriubogdan"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          dulgheriubogdan
+        </a>
+      </h1>
+
+      <h1>
+        Calin Silvian Petre -
+        <a
+          href="https://github.com/xRankk"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          xRankk
+        </a>
+      </h1>
     </div>
   );
 }
