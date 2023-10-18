a7e02f3c09f51d3bfc39feb7cddafa1f14b62c6b
dulgheriubogdan <dulgheriubogdan30@gmail.com>
2023-10-18 22:39:37 +0300

    Added new file for separation

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,12 @@
 import "./App.css";
+import ListaVoluntari from "./ListaVoluntari";
 
 function App() {
   return (
     <div className="App">
       <div className="App-header">
         <div className="titlu">Voluntari Departament IT 2023-2024</div>
-        <div className="scrollable-div">
-          <h1>Nume si prenume - Nume github</h1>
-          <h1>Theo Storm - $efu</h1>
-          <h1>Cristi Milmoiu - Technic bo$$</h1>
-          <h1>Boscanici Adrian - O secreta pe mata</h1>
-          <h1>Dulgheriu Bogdan Gabriel - dulgheriubogdan</h1>
-          <h1>Calin Silvian Petre - xRankk</h1>
-          <h1>test - test</h1>
-          <h1>test - test</h1>
-          <h1>test - test</h1>
-          <h1>test - test</h1>
-          <h1>test - test</h1>
-          <h1>test - test</h1>
-          <h1>test - test</h1>
-          <h1>test - test</h1>
-          <h1>test - test</h1>
-          <h1>test - test</h1>
-          <h1>test - test</h1>
-        </div>
+        <ListaVoluntari />
       </div>
     </div>
   );
---
+++ client/src/ListaVoluntari.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function ListaVoluntari() {
+  return (
+    <div className="scrollable-div">
+      <h1>Boscanici Adrian - O secreta pe mama ta</h1>
+      <h1>Dulgheriu Bogdan Gabriel - dulgheriubogdan</h1>
+      <h1>Calin Silvian Petre - xRankk</h1>
+    </div>
+  );
+}
+
+export default ListaVoluntari;
