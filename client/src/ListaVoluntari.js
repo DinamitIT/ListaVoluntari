3d305f40c5bf995717dbbf70f9fcd001cc43d199
dulgheriubogdan <dulgheriubogdan30@gmail.com>
2023-10-18 22:40:50 +0300

    Added new file for separtion

--- client/src/ListaVoluntari.js
+++ client/src/ListaVoluntari.js
@@ -3,6 +3,7 @@ import "./App.css";
 function ListaVoluntari() {
   return (
     <div className="scrollable-div">
+      <h1>Nume Prenume - Nume GitHub</h1>
       <h1>Boscanici Adrian - O secreta pe mama ta</h1>
       <h1>Dulgheriu Bogdan Gabriel - dulgheriubogdan</h1>
       <h1>Calin Silvian Petre - xRankk</h1>
