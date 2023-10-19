1c4768be9204662dee268ccb670d183a822c9de4
Cristi Miloiu <miloiuc4@gmail.com>
2023-10-19 19:08:34 +0300

    nume

--- client/src/ListaVoluntari.js
+++ client/src/ListaVoluntari.js
@@ -4,6 +4,7 @@ export default function ListaVoluntari() {
       <h1>Dulgheriu Bogdan Gabriel - dulgheriubogdan</h1>
       <h1>Calin Silvian Petre - xRankk</h1>
       <h1>Cristi Miloiu - cristim67</h1>
+      <h1>Mihnea Aniculesei - amihneaa</h1>
     </>
   );
 }
